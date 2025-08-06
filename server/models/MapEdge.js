dc3aaf4e7930c814b0f40655a67fc7c50593d73a
pucha02 <puchko.v.keykey@gmail.com>
2025-08-06 19:59:43 +0300

    Delete server directory

--- server/models/MapEdge.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const MapEdgeSchema = new mongoose.Schema({
-  from: { type: String, required: true },
-  to: { type: String, required: true },
-  polyline: [{ x: Number, y: Number }],
-  name: String, // название маршрута (может быть пустым)
-});
-export default mongoose.model("MapEdge", MapEdgeSchema);
